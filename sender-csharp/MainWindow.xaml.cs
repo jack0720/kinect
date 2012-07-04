69ebe5aee6d119fa23c44f6fc279aac674b5fac3
unknown <cs11002@cs11002-PC.(none)>
2012-07-04 17:01:09 +0900

    Signed-off-by: unknown <cs11002@cs11002-PC.(none)>

--- sender-csharp/MainWindow.xaml.cs
+++ sender-csharp/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -14,6 +16,8 @@ using System.Windows.Shapes;
 using System.Windows.Threading;
 using WebSocket4Net;
 using Microsoft.Kinect;
+using Microsoft.Speech.AudioFormat;
+using Microsoft.Speech.Recognition;
 
 namespace sender_csharp
 {
@@ -30,12 +34,19 @@ namespace sender_csharp
         private WebSocket websocket;
         private bool ready = false;
 
+        /// <summary>
+        /// Active Kinect sensor.
+        /// </summary>
+        private KinectSensor sensor;
         KinectSensor kinect;
-
+        /// <summary>
+        /// Speech recognition engine using audio data from Kinect.
+        /// </summary>
+        private SpeechRecognitionEngine speechEngine;
+        
         public MainWindow()
         {
             InitializeComponent();
-
             //Kinectの初期化
             kinect = KinectSensor.KinectSensors[0];
 
@@ -45,7 +56,6 @@ namespace sender_csharp
             kinect.SkeletonStream.Enable();
 
             kinect.Start();
-
             if (serverURL == "")
             {
                 textBox1.Text = "URL不明！";
@@ -71,7 +81,7 @@ namespace sender_csharp
                 websocket.Send(channel + ":" + cmd + "," + msg);
             }
         }
-     
+
         private void websocket_Opened(object sender, EventArgs e)
         {
             //以下のブロックはスレッドセーフにGUIを扱うおまじない
@@ -158,26 +168,163 @@ namespace sender_csharp
             }
         }
 
-            void handler_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        void handler_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            SkeletonFrame temp = e.OpenSkeletonFrame();
+            if (temp != null)
             {
-                SkeletonFrame temp = e.OpenSkeletonFrame();
-                if (temp != null)
+                Skeleton[] skeletonData = new Skeleton[temp.SkeletonArrayLength];
+                temp.CopySkeletonDataTo(skeletonData);
+                foreach (Skeleton skeleton in skeletonData)
                 {
-                        Skeleton[] skeletonData = new Skeleton[temp.SkeletonArrayLength];
-                        temp.CopySkeletonDataTo(skeletonData);
-                         foreach (Skeleton skeleton in skeletonData)
-                        {
-                            if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
-                            {
-                                Joint right = skeleton.Joints[JointType.HandRight];
-                                Console.WriteLine(right.Position.X + "    " + right.Position.Y + "    " + right.Position.Z + "\n");
-                                    if(right.Position.Z < 1)
-                                    {
-                                    sendMessage("mouse", (300 + right.Position.X*300) + "," + (200 + right.Position.Y*-300));
-                                    }
-                            }
-                        }
+                    if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
+                    {
+                        Joint right = skeleton.Joints[JointType.HandRight];
+                        Joint left = skeleton.Joints[JointType.HandLeft];
+                        Console.WriteLine("right" + right.Position.X + "    " + right.Position.Y + "    " + right.Position.Z + "\n");
+                        Console.WriteLine("left" + left.Position.X + "    " + left.Position.Y + "    " + left.Position.Z + "\n");
+                        Console.WriteLine((400 + right.Position.X * 800) + "," + (200 + right.Position.Y * -400) + "," + (400 + left.Position.X * 800) + "," + (200 + left.Position.Y * -400) + "\n");
+                        sendMessage("hand", (600 + right.Position.X * 1200) + "," + (400 + right.Position.Y * -800) + "," + (600 + left.Position.X * 1200) + "," + (400 + left.Position.Y * -800));
                     }
                 }
             }
+        }
+
+        //個々以下音声
+        private static RecognizerInfo GetKinectRecognizer()
+        {
+            foreach (RecognizerInfo recognizer in SpeechRecognitionEngine.InstalledRecognizers())
+            {
+                string value;
+                recognizer.AdditionalInfo.TryGetValue("Kinect", out value);
+                if ("True".Equals(value, StringComparison.OrdinalIgnoreCase) && "ja-JP".Equals(recognizer.Culture.Name, StringComparison.OrdinalIgnoreCase))
+                {
+                    return recognizer;
+                }
+            }
+
+            return null;
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            // Look through all sensors and start the first connected one.
+            // This requires that a Kinect is connected at the time of app startup.
+            // To make your app robust against plug/unplug, 
+            // it is recommended to use KinectSensorChooser provided in Microsoft.Kinect.Toolkit
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (null != this.sensor)
+            {
+                try
+                {
+                    // Start the sensor!
+                    this.sensor.Start();
+                }
+                catch (IOException)
+                {
+                    // Some other application is streaming from the same Kinect sensor
+                    this.sensor = null;
+                }
+            }
+
+            if (null == this.sensor)
+            {
+                this.textBlock3.Text = "Kinect の準備ができてません";
+                return;
+            }
+
+            RecognizerInfo ri = GetKinectRecognizer();
+
+            if (null != ri)
+            {
+                // 認識用の辞書を登録する
+                this.speechEngine = new SpeechRecognitionEngine(ri.Id);
+                var directions = new Choices();     //  よみ　コマンド名
+                directions.Add(new SemanticResultValue("はじめ", "start"));
+                var gb = new GrammarBuilder { Culture = ri.Culture };
+                gb.Append(directions);
+
+                var g = new Grammar(gb);
+                speechEngine.LoadGrammar(g);
+
+                speechEngine.SpeechRecognized += SpeechRecognized;
+                speechEngine.SpeechRecognitionRejected += SpeechRejected;
+
+                speechEngine.SetInputToAudioStream(
+                    sensor.AudioSource.Start(), new SpeechAudioFormatInfo(EncodingFormat.Pcm, 16000, 16, 1, 32000, 2, null));
+                speechEngine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            else
+            {
+                this.textBlock3.Text = "Kinect 音声認識の初期化に失敗しました";
+            }
+        }
+
+        /// <summary>
+        /// Execute uninitialization tasks.
+        /// </summary>
+        /// <param name="sender">object sending the event.</param>
+        /// <param name="e">event arguments.</param>
+        private void WindowClosing(object sender, CancelEventArgs e)
+        {
+            if (null != this.sensor)
+            {
+                this.sensor.AudioSource.Stop();
+
+                this.sensor.Stop();
+                this.sensor = null;
+            }
+
+            if (null != this.speechEngine)
+            {
+                this.speechEngine.SpeechRecognized -= SpeechRecognized;
+                this.speechEngine.SpeechRecognitionRejected -= SpeechRejected;
+                this.speechEngine.RecognizeAsyncStop();
+            }
+        }
+        /// <summary>
+        /// Handler for recognized speech events.
+        /// </summary>
+        /// <param name="sender">object sending the event.</param>
+        /// <param name="e">event arguments.</param>
+        private void SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            // Speech utterance confidence below which we treat speech as if it hadn't been heard
+            const double ConfidenceThreshold = 0.3;
+
+            this.textBlock5.Text = "";
+
+            if (e.Result.Confidence >= ConfidenceThreshold)
+            {
+                String command = e.Result.Semantics.Value.ToString();
+                switch (command)
+                {
+                        // 認識された結果を WebSocket で送信する
+                    case "start":
+                        this.textBlock5.Text = "voice: " + command;
+                        sendMessage("voice", "voice," + command);
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handler for rejected speech events.
+        /// </summary>
+        /// <param name="sender">object sending the event.</param>
+        /// <param name="e">event arguments.</param>
+        private void SpeechRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            this.textBlock5.Text = "";
+        }
+
     }
+}
